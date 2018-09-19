d60b8db2171f4d59b9f7edddd4ec557710f95bf5
s-gaor <s-gaor@53S50030707.it.bsd405.org>
2018-09-19 09:37:40 -0700

    This is my ASCII Art project!

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -15,3 +15,41 @@
  */
 
 // YOUR CODE GOES HERE :)
+
+public class ASCIIArt {
+		//This is a Panda!!!!
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello, I'm a Panda and I love eating bamboo!!!\n");
+		ears ();
+		head ();
+		body ();
+		legs ();
+	}
+	public static void ears () {
+		System.out.println("      ___         ___   __");
+		System.out.println("     /|||\\       /|||\\ |__|/|");
+		System.out.println("     \\UUU/_______\\UUU/ |  |/ ");
+
+	}
+	public static void head () {
+		System.out.println("        /         \\  |\\|__|");
+		System.out.println("       /           \\  \\|  | ");
+		System.out.println("       | (^) . (^) |   |__|/| ");
+		System.out.println("        \\_________/    |  |/");
+	}
+	public static void body () {
+		System.out.println("     ___/|||||||||\\___ |__|");
+		System.out.println("    (||/           \\||)|  |");
+		System.out.println("      /             \\  |__|/| ");
+		System.out.println("      |      x      ||\\|  |/");
+		System.out.println("       \\___________/  \\|__|");
+
+	}
+	public static void legs () {
+		System.out.println("         /|||^|||\\     |  |");
+		System.out.println("      __/|||/ \\|||\\__  |__|");
+		System.out.println("     ||||||/   \\|||||| |__|  ");
+		
+	}
+}
